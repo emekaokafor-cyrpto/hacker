6d245a564e5e696f1e66e7d3ceb561d40b73d1dd
emekaokafor-cyrpto <emeka.okafor@binghamuni.edu.ng>
2025-12-03 00:26:24 +0300

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,13 @@
+print(".........welcome to formula chooser.........")
+print("1. formula A\n2. formula B\n3. formula C\n4. formula D")
+choice = input("Enter your choice from (1-4): ")
+if choice == "1":
+    print("your using formula A")
+elif choice == "2":
+    print("your using formula B")
+elif choice == "3":
+    print("your using formula C")
+elif choice == "4":
+    print("your using formula D")
+else:
+    print("invalid choice")
